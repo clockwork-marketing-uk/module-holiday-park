a1d2d8b55a2452f09a8667997a38b6e5dbf9c59c
Tom Allenbrook <tom.allenbrook@clock-work.co.uk>
2024-03-08 12:49:15 +0000

    pretty print json debug

--- src/resources/views/holiday-park/booking/book.blade.php
+++ src/resources/views/holiday-park/booking/book.blade.php
@@ -7,7 +7,9 @@
     <h1>BOOK NOWW</h1>
 
     @php
-        echo "<pre>".json_encode($stay, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)."</pre>";
+        echo '<pre>' .
+            json_encode($stay, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) .
+            '</pre>';
     @endphp
 
 @endsection
